107440199058c088ad07b907a53bfb7228a76269
Ne_Uchi <drmor107@gmail.com>
2026-03-05 23:53:17 +0500

    now all data goes from JS

--- script.js
+++ script.js
@@ -2,16 +2,24 @@ const temp = document.querySelector('.temp');
 const info = document.querySelector('.info');
 const tempInfo = document.querySelector('.tempInfo');
 const cityName = document.querySelector('.cityName');
+const descriptionP = document.querySelector('.des');
+const humidity = document.querySelector('.hum');
+const conditions = document.querySelector('.con');
+const windSpeed = document.querySelector('.ws');
 
 async function getTemp() {
   try {
     const response = await fetch(
-      'https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/atyrau?key=FJ3S8FJ4ZJHBBD6CQN5GXKVQD',
+      'https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/almaty?key=FJ3S8FJ4ZJHBBD6CQN5GXKVQD',
     );
     const cityData = await response.json();
     const cel = Math.floor((cityData.currentConditions.temp - 32) * (5 / 9));
     tempInfo.innerText = `${cel} °C`;
     cityName.textContent = cityData.address;
+    descriptionP.textContent = cityData.description;
+    humidity.textContent = cityData.currentConditions.humidity;
+    conditions.textContent = cityData.currentConditions.conditions;
+    windSpeed.textContent = cityData.currentConditions.windspeed;
     console.log(cityData);
   } catch (error) {
     console.error(error);
